8cdf6a22f6a841ee2e1369c564b367cc39704fcc
tuxskar <tuxskar@gmail.com>
2017-02-14 22:11:14 +0100

    Adding better error handling and added more debugigng msgs

--- telegram_writer.py
+++ telegram_writer.py
@@ -6,6 +6,7 @@ import time
 import traceback
 
 import telegram
+from telegram.error import BadRequest
 
 import settings
 
@@ -18,23 +19,32 @@ class TelegramWriter:
     def run(self, bots, wait_seconds):
 
         while True:
+            print('\nStarting a bunch of checks {}'.format(datetime.datetime.utcnow()))
             for bot in bots:
                 if settings.DEBUG:
-                    print('Checking for news {}'.format(datetime.datetime.utcnow()))
+                    print('Checking for news {} on bot type {}-{}'.format(datetime.datetime.utcnow(), bot.web_type,
+                                                                          bot.target_url))
                 chat_id = bot.chat_id or self.chat
                 try:
                     posts = bot.get_last_updates()
+                    if settings.DEBUG:
+                        print('-- {} New posts found'.format(len(posts)))
                     for post in posts:
                         self.telegram.sendMessage(chat_id=chat_id, text=post.description)
                         if post.image:
-                            self.telegram.sendPhoto(chat_id=chat_id, photo=post.image)
+                            try:
+                                self.telegram.sendPhoto(chat_id=chat_id, photo=post.image)
+                            except BadRequest:
+                                pass
+                    if len(posts):
+                        time.sleep(5)  # 5 seconds to don't kill the telegram API
                 except Exception as e:
                     exc_type, exc_value, exc_traceback = sys.exc_info()
                     error_stack = "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
-                    print(error_stack)
                     if settings.SEND_ERRORS_BY_TELEGRAM:
                         try:
                             self.telegram.sendMessage(chat_id=settings.ERRORS_TELEGRAM_CHAT_ID, text=error_stack)
                         except Exception:
                             pass
+                    print(error_stack)
             time.sleep(wait_seconds)
