130fd1497bc93315962c9688cae5c83757e85e6c
Oscar Ramirez <tuxskar@gmail.com>
2017-02-17 21:24:38 +0100

    Added Vibbo crawler

---
+++ crawlers/vibbo_crawler.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+
+
+import requests
+from bs4 import BeautifulSoup
+
+from crawlers.crawler_base import Crawler
+from db.models import Post
+
+
+class VibboCrawler(Crawler):
+    """
+    Crawler specific to gather the information from the web www.vibbo.com old segundamano.es
+    """
+    crawler_url = 'https://www.vibbo.com'
+    web_type = 'VIBBO'
+
+    def __init__(self, target_url, *args, **kwargs):
+        super(VibboCrawler, self).__init__(*args, **kwargs)
+        self.target_url = target_url
+
+    def get_last_posts(self):
+        """
+        Specific implementation of the generic get_last_posts to gather the last
+        post in the web
+        :return: [Post]
+        """
+        last_posts = []
+        r = requests.get(self.target_url)
+        html = BeautifulSoup(r.content, 'html.parser')
+        raw_posts = html.findAll("div", {"class": "list_ads_row"})
+
+        for post in raw_posts:
+            title_element = post.find("a", {"class": "subjectTitle"})
+            title = cleaning_spaces(title_element.text)
+            href = title_element['href']
+            description = ' -- '.join([post.find("p", {"class": "zone"}).text, post.find("p", {"class": "date"}).text])
+            id_post = int(post['id'])
+            price_full = cleaning_spaces(post.find("a", {"class": "subjectPrice"}).text)
+            price = int(price_full[:-1].replace('.', ''))  # Removing currency symbol
+            image_element = post.find('img', {'class': 'lazy'})
+            image_src = None
+            if image_element:
+                image_src = image_element.get('title', '')
+            complete_href = href
+            description = '\n'.join([title, price_full, cleaning_spaces(description), complete_href])
+            last_posts.append(Post(id=id_post, href=complete_href, description=description, image=image_src,
+                                   price=price))
+        return last_posts
+
+
+def cleaning_spaces(txt):
+    return ' '.join(txt.split())
--- run-crawler-sampler.py
+++ run-crawler-sampler.py
@@ -1,21 +1,24 @@
 """
 This is another example how to use the crawlers using a list of pairs (url, chat_id) to setup them
 """
-
-from settings import TELEGRAM_TOKEN, BOT_CHAT_ID, CHECKING_TIMEOUT, TYPE_URL_N_CHAT
-from telegram_writer import TelegramWriter
-from crawlers.enalquiler_crawler import EnalquilerCrawler
-from crawlers.idealista_crawler import IdealistaCrawler
 from crawlers.milanuncios_crawler import MilanunciosCrawler
+from crawlers.vibbo_crawler import VibboCrawler
+from settings import TELEGRAM_TOKEN, BOT_CHAT_ID, CHECKING_TIMEOUT
+from telegram_writer import TelegramWriter
 
 
 if __name__ == '__main__':
     bots = []
-    crawlers = [('enalquiler', EnalquilerCrawler),
-                ('idealista', IdealistaCrawler),
-                ('milanuncios', MilanunciosCrawler)]
-    for crawler_key, CrawlerInstance in crawlers:
-        for url, chat_id in TYPE_URL_N_CHAT[crawler_key]:
-            bots.append(CrawlerInstance(url, chat_id))
+    # Vibbo example
+    url = 'http://www.vibbo.com/portatiles-netbooks-de-segunda-mano-malaga/mac-pro.htm?ca=29_s&fPos=402&fOn=sl1'
+
+    chat_id = '-219998589'  # change for your chat id where your bot can write on
+    bots.append(VibboCrawler(url, chat_id))
+
+    # Milanuncios example
+    url = 'http://www.milanuncios.com/portatiles-de-segunda-mano-en-malaga/mac-pro.htm'
+    bots.append(MilanunciosCrawler(url, chat_id))
+
+    # Instantiating the TelegraWriter with the example crawlers
     telegram = TelegramWriter(TELEGRAM_TOKEN, BOT_CHAT_ID or None)
     telegram.run(bots, wait_seconds=CHECKING_TIMEOUT)
